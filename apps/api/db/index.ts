f29dd46282fcb2baddb716560ad57a0d7725318b
Aidan Froggatt <aidanfr@live.ca>
2025-09-24 15:33:07 -0400

    fixing env imports

--- apps/api/db/index.ts
+++ apps/api/db/index.ts
@@ -1,6 +1,9 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import path from "path";
+import { config } from "dotenv";
+
+config({ path: path.resolve(__dirname, "../../../.env") });
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in root .env file");
--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,5 +1,8 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import type { Config } from "drizzle-kit";
+import path from "path";
+
+config({ path: path.resolve(__dirname, "../../.env") });
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set in root .env file");
