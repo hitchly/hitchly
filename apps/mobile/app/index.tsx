056d7c210f74c78a371d809e51be142d6e3d2814
Aidan Froggatt <aidanfr@live.ca>
2026-01-02 18:20:37 -0500

    fixing lint warning

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { trpc } from "../utils/trpc";
 
 export default function UsersScreen() {
