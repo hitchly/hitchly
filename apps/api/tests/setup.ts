751fae655af8cd4def4d17fdfaf5ab4478b22b0f
Aidan Froggatt <aidanfr@live.ca>
2026-02-13 17:05:25 -0500

    updating test setup

---
+++ apps/api/tests/setup.ts
@@ -0,0 +1,4 @@
+process.env.GMAIL_USER = "test@hitchly.com";
+process.env.GMAIL_APP_PASS = "dummy_app_password";
+process.env.STRIPE_SECRET_KEY = "sk_test_mock_key";
+process.env.GOOGLE_MAPS_API_KEY = "mock_google_maps_key";
